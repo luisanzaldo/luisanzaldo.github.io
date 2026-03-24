3f7647591abb187e029d0c99e9de2657534780b2
luisanzaldo <luis.anzaldo@acontraflujo.com>
2026-03-23 19:12:57 -0600

    Optimización de página

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	build: {
+		inlineStylesheets: "always",
+	},
 	vite: {
 		server: {
 			watch: {
